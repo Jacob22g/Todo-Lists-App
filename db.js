5ca26c78b5a3f3ca142020afe2f9d6096efc4bd2
Jacob Graham <jacob.22.g@gmail.com>
2021-03-28 13:26:45 +0300

    first commit

--- db.js
+++ db.js
@@ -1,11 +1,18 @@
 const Pool = require('pg').Pool
+require('dotenv').config()
 
-const pool = new Pool({
-    user: process.env.PGUSER || "postgres",
-    password: process.env.PGPASSWORD || "pp12345",
-    host: process.env.PGHOST || "localhost",
-    port: process.env.PGPORT || 5432,
-    database: process.env.PGDATABASE || "perntodo"
-})
+const devConfig = {
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE
+}
+
+const proConfig = {
+    connectionString: process.env.DATABASE_URL // heroku addons
+}
+
+const pool = new Pool(process.env.NODE_ENV === 'production' ? proConfig : devConfig)
 
 module.exports = pool
--- index.js
+++ index.js
@@ -2,17 +2,29 @@ const express = require('express')
 const cors = require('cors')
 const listRouter = require('./routers/list')
 const taskRouter = require('./routers/task')
+const path = require('path')
 
 const app = express()
 const port = process.env.PORT || 5000
 
 //middleware 
 app.use(cors())
-app.use(express.json()) // req.body
+app.use(express.json()) // allow access to req.body
+
+// app.use(express.static(path.join(__dirname, 'client/build')))
+
+if(process.env.NODE_ENV === "production"){
+    //serve static content
+    app.use(express.static(path.join(__dirname, 'client/build')))
+}
 
 //ROUTES//
 app.use(listRouter)
 app.use(taskRouter)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'))
+})
 
 app.listen(port, () => {
     console.log('server is runing on port '+port)
